b5cd9ed60d53283f96335fa2a0f876945d5f4126
inhs <harrysudana@gmail.com>
2023-02-20 12:51:42 +0800

    saving chirps https://bootcamp.laravel.com/blade/creating-chirps#saving-the-chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
-use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
@@ -31,7 +31,15 @@ class ChirpController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+
+            'message' => 'required|string|max:255',
+
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
